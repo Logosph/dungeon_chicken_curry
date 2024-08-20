c751a1f994d41ae0820840e505279ffbc3ab9fab
Julia Kugut <kugut.yulya@mail.ru>
2024-08-20 23:46:46 +0300

    rename adapter from creat eroom1adaper to ChoiseListDialogAdapter

--- app/src/main/java/ru/logosph/dungeon_chicken_curry/ui/dialogs/ChoiseListDialogAdapter.kt
+++ app/src/main/java/ru/logosph/dungeon_chicken_curry/ui/dialogs/ChoiseListDialogAdapter.kt
@@ -1,16 +1,15 @@
-package ru.logosph.dungeon_chicken_curry.ui.fragments.create_room_1
+package ru.logosph.dungeon_chicken_curry.ui.dialogs
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import ru.logosph.dungeon_chicken_curry.R
 import ru.logosph.dungeon_chicken_curry.databinding.ItemForListBinding
 import ru.logosph.dungeon_chicken_curry.domain.local_data.RaceAndClassModel
 
 // TODO: Rename to Choose List DF and relocate it to dialogs
-class CreateRoom1Adapter(
+class ChoiseListDialogAdapter(
     private val data: MutableList<RaceAndClassModel> = mutableListOf()
-) : RecyclerView.Adapter<CreateRoom1Adapter.ViewHolder>() {
+) : RecyclerView.Adapter<ChoiseListDialogAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: ItemForListBinding) : RecyclerView.ViewHolder(binding.root)
 
--- app/src/main/java/ru/logosph/dungeon_chicken_curry/ui/dialogs/ChoiseListDialogFragment.kt
+++ app/src/main/java/ru/logosph/dungeon_chicken_curry/ui/dialogs/ChoiseListDialogFragment.kt
@@ -5,13 +5,9 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.google.android.material.carousel.CarouselLayoutManager
-import com.google.android.material.carousel.CarouselSnapHelper
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import ru.logosph.dungeon_chicken_curry.R
 import ru.logosph.dungeon_chicken_curry.databinding.DialogChoiseListBinding
-import ru.logosph.dungeon_chicken_curry.ui.fragments.create_room_1.CreateRoom1Adapter
-import ru.logosph.dungeon_chicken_curry.ui.fragments.create_room_2.CreateRoom2Adapter
 
 class ChoiseListDialogFragment : DialogFragment() {
 
@@ -26,7 +22,7 @@ class ChoiseListDialogFragment : DialogFragment() {
 
         // TODO: Change carousel LM to Grid LM to Yulia
         with(binding) {
-            val adapter = CreateRoom1Adapter()
+            val adapter = ChoiseListDialogAdapter()
             listRecyclerView.adapter = adapter
             listRecyclerView.layoutManager = GridLayoutManager(context, 2)
 
